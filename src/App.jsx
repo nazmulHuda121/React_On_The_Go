bb2892e2c49d21587e69976ee9965417604df905
nazmul_khan <nazmul.khan121@gmail.com>
2025-09-18 21:23:10 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Countries from './Components/Countries/Countries';
+
+function App() {
+  return (
+    <>
+      <h1 className="text-green-300">React World on the go...</h1>
+      <Countries />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+  return (
+    <div>
+      <h2 className="text-red-600">Countries List: </h2>
+    </div>
+  );
+};
+
+export default Countries;
